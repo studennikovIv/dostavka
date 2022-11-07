26155e8ef84a1aeab72f5dd9ae933abd889eabfe
grant956 <68757554+studennikovIv@users.noreply.github.com>
2022-11-07 18:52:36 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import css from 'App.module.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>asdasdasd</h1>
+        </header>
+        <div className={css.flex}>asd</div>
+      </>
+    );
+  }
+}
